14653f43d7b88fd7b98a1d4eb991317c859f6f83
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-13 22:31:55 +0800

    Update Searching for Position

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 EMAIL = os.environ['LINKEDIN_EMAIL']
 PASSWORD = os.environ['LINKEDIN_PASSWORD']
 SIGN_IN_URL = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin'
-JOB_SEARCH_URL = 'https://www.linkedin.com/jobs/search/?currentJobId=3664968393&f_AL=true&geoId=103121230&keywords=Python%20Intern&location=Philippines&refresh=true'
+JOB_SEARCH_URL = 'https://www.linkedin.com/jobs/search/'
+POSITION = 'Python Intern'
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 import config
 
 # Using Selenium with a webdriver_manager
@@ -22,6 +23,17 @@ password_input.send_keys(Keys.ENTER)
 # Go to the Job Posting
 driver.get(url=config.JOB_SEARCH_URL)
 
+# Search for your position
+search_bar = driver.find_element(By.XPATH, '//*[@id="jobs-search-box-keyword-id-ember24"]')
+search_bar.send_keys(config.POSITION)
+search_bar.send_keys(Keys.ENTER)
+
+# Click the Easy Apply on the Job Posting
+div_container = driver.find_element(By.XPATH,
+                                    '//*[@id="main"]/div/div[2]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[1]/div['
+                                    '4]/div/div/div')
+easy_apply = div_container.find_element(By.TAG_NAME, 'button')
+easy_apply.click()
 
 time.sleep(100)
 
